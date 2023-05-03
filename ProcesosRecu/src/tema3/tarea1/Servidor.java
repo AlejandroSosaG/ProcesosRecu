b743067607605ba4df5295c83b87095438f04b08
Usuario <alejandro.sosa@iesnervion.es>
2023-05-03 20:24:21 +0200

    Tema 3

---
+++ ProcesosRecu/src/tema3/tarea1/Servidor.java
@@ -0,0 +1,41 @@
+package tema3.tarea1;
+
+import java.io.*;
+import java.net.*;
+
+public class Servidor {
+    public static void main(String[] args) {
+        String contenido = null;
+        System.out.println("Sevidor: Abriendo conexión");
+        ServerSocket socketServidor = null;
+        try {
+            socketServidor = new ServerSocket(1500);
+            System.out.println("Sevidor: aceptando conexión");
+            Socket socketCliente = socketServidor.accept();
+            System.out.println("Sevidor: Abriendo flujos de entrada y salida");
+            InputStream is = socketCliente.getInputStream();
+            OutputStream os = socketCliente.getOutputStream();
+            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+            BufferedReader br = new BufferedReader(isr);
+            File archivo = new File (br.readLine());
+            FileReader fr = new FileReader (archivo);
+            String linea;
+            while ((linea = br.readLine())!=null) contenido += linea;
+            System.out.println("Sevidor envia mensaje al cliente");
+            OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+            BufferedWriter bw = new BufferedWriter(osw);
+            bw.write(contenido);
+            bw.newLine();
+            bw.flush();
+            fr.close();
+            br.close();
+            isr.close();
+            is.close();
+            os.close();
+            socketCliente.close();
+            socketServidor.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
